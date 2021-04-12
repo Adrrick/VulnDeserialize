56373a2ffe7dcb3e985da93cde4e312dae0f3e97
Tristan Lefebvre <tristan.lefebvre@ynov.com>
2021-04-12 13:49:31 +0200

    Ajoutez des fichiers projet.

---
+++ Controllers/VulnerableController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VulnDeserialize.Models;
+
+namespace VulnDeserialize.Controllers
+{
+    [Route("api/[controller]")]
+    public class VulnerableController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] Info value)
+        {
+            if (value == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Models/Info.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VulnDeserialize.Models
+{
+    public class Info
+    {
+        public string Name { get; set; }
+        public dynamic Object { get; set; }
+    }
+}
